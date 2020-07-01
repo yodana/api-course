64df0db36bf86171f0f645cde0f4c91a0295efef
DANA <dana@MacBook-Air-de-DANA.local>
2020-07-01 17:00:30 +0200

    try 3 to fix doctrine problem (delete doctrine/bundle)

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,6 +8,7 @@ use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
+use Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle;
 
 class Kernel extends BaseKernel
 {
