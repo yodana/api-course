91273cf3c02cdf3213104ee8de2f0fba580286de
DANA <dana@MacBook-Air-de-DANA.local>
2020-07-01 17:48:17 +0200

    try 3 to fix doctrine problem (delete doctrine/bundle)

--- config/bundles.php
+++ config/bundles.php
@@ -15,7 +15,6 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
